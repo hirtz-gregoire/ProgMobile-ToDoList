7d8c906cf1a1f4ddae872e9f61caf8e732de2b82
GregoireHirtz <hirtz.gregoire@ghmail.fr>
2024-06-04 15:37:40 +0200

    etape1 1-10

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/todo_list_app.dart';
 
 void main() {
-
+  runApp(ToDoListApp());
 }
---
+++ lib/todo_list_app.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ToDoListApp extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ToDoListAppState();
+  }
+
+}
+
+
+
+class _ToDoListAppState extends State<ToDoListApp>{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ToDo List'),
+        ),
+        body: Center(
+          child: Text('Hello, world!'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Action à définir
+          },
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
