814264b74684e2f856d7ecb3a520680d1e37c52e
GregoireHirtz <hirtz.gregoire@ghmail.fr>
2024-06-12 14:14:14 +0200

    etape1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:todolist/tasks_master.dart';
 import 'package:todolist/todo_list_app.dart';
 
 void main() {
-  runApp(TasksMaster());
+  runApp(ToDoListApp());
 }
